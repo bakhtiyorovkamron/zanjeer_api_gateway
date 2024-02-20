1d21ef406dab8e58f49225e2c76ee524f175119c
HLIQKamron <bahtiyorovkamron8@gmail.com>
2024-02-20 15:50:25 +0500

    user register

--- api/router.go
+++ api/router.go
@@ -36,10 +36,10 @@ func New(cfg config.Config, strg storage.StorageI, log *logger.Logger) *gin.Engi
 	})
 	admin := r.Group("/admin")
 	admin.POST("/login", h.Login)
-	// admin.Use(h.JwtAuthMiddleware())
-	// admin.Use(h.SuperAdminCheckType())
-	admin.POST("/add/admin", h.CreateAdmin)
+	admin.Use(h.JwtAuthMiddleware())
 	admin.GET("/get/admins", h.GetAdmins)
+	admin.Use(h.SuperAdminCheckType())
+	admin.POST("/add/admin", h.CreateAdmin)
 
 	user := r.Group("/user")
 	user.POST("/register", h.UserRegister)
